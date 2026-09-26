a8a4e154610721f31728d4cb26087efd89081f0d
Steven Mejia <boyishtomb519@gmail.com>
2026-09-26 14:53:45 -0400

    Fixed small font and yellow background of edit contact button

--- js/code.js
+++ js/code.js
@@ -302,9 +302,9 @@ function searchContacts()
             <span class="text-muted small me-2">${c.Phone}</span>
             <button type="button-${contactId}" class="btn-close btn-close-white" style="font-size: 0.65rem;" onclick="deleteContact(${contactId ? contactId : `'${contactName.replace(/'/g, "\\'")}'`});" title="Delete Contact"></button>
             <button type="button" id="editButton-${contactId}" 
-            class="btn btn-warning btn-sm p-0 rounded-0 d-flex align-items-center justify-content-center" 
+            class="btn btn-link text-body p-0 ms-2 text-decoration-none d-inline-flex align-items-center" 
             style="width: 20px; height: 20px;" onclick="toggleEditForm(${contactId });" title="Edit Contact">
-              <i class="bi bi-pencil-fill" style="font-size: 0.55rem;">Edit Contact</i>
+              <i class="bi bi-pencil-fill" style="font-size: 0.85rem;">Edit Contact</i>
             </button>
           </span>
           <div id="editForm-${contactId}" class="card card-body mt-2 shadow-sm d-none" style="max-width: 320px;">
